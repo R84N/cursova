d25c8a9e1eef1b3fec62c351958de1b764c94103
R84N <misha3001011@gmail.com>
2025-04-14 13:34:03 +0300

    fsdf

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,25 +5,24 @@ import EmptyOrg from "./_components/EmptyOrg";
 import { useOrganization } from "@clerk/nextjs";
 import BoardList from "./_components/BoardList";
 
-
-interface DashboardPageProps  {
-  searchParams: { [key: string]: string | string[] | undefined }
+interface DashboardPageProps {
+  params: { [key: string]: string }  // додаємо
+  searchParams?: { [key: string]: string | string[] | undefined } // робимо optional
 }
 
-const DashboardPage = ({searchParams}:DashboardPageProps) => {
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
 
-  const {organization} = useOrganization();
+  const { organization } = useOrganization();
 
   return (
-  <div className="flex-1 h-[calc(100%-76px)]">
-    {!organization?(
-      <EmptyOrg />
-    ):(
-      <BoardList orgId={organization.id} query={searchParams} />
-    )}
-   
-  </div>
-);
+    <div className="flex-1 h-[calc(100%-76px)]">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams ?? {}} />
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
