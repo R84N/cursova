e8b9fe7414e3b001444d84c471a7b1a813165be7
Misha <misha3001011@gmail.com>
2025-04-17 14:58:24 +0300

    fsdf

--- app/board/[boardId]/_components/SelectionTools.tsx
+++ app/board/[boardId]/_components/SelectionTools.tsx
@@ -8,7 +8,7 @@ import ColorPicker from "./ColorPicker";
 import { useDeleteLayers } from "@/hooks/use-delete-layers";
 import { Button } from "@/components/ui/button";
 import Hint from "@/components/hint";
-import { Trash2} from "lucide-react";
+import { BringToFront, SendToBack, Trash2} from "lucide-react";
 
 interface SelectionToolsProps{
     camera: camera,
@@ -17,6 +17,42 @@ interface SelectionToolsProps{
 
 const SelectionTools = memo(({camera, setLastUsedColor}:SelectionToolsProps) => {
     const selection = useSelf((me)=> me.presence.selection )
+
+    const moveToBack = useMutation(({storage})=>{
+        const liveLayersIds = storage.get("layerIds");
+        const indices:number[] = [];
+
+        const arr = liveLayersIds.toArray();
+
+        for(let i= 0; i<arr.length; i++){
+            if(selection?.includes(arr[i])){
+                indices.push(i);
+            }
+        }
+
+
+        for(let i = 0; i < indices.length; i++){
+            liveLayersIds.move(indices[i],i);
+        }
+    },[selection])
+
+    const moveToFront = useMutation(({storage})=>{
+        const liveLayersIds = storage.get("layerIds");
+        const indices:number[] = [];
+
+        const arr = liveLayersIds.toArray();
+
+        for(let i= 0; i<arr.length; i++){
+            if(selection?.includes(arr[i])){
+                indices.push(i);
+            }
+        }
+
+
+        for(let i = indices.length - 1; i >= 0; i++){
+            liveLayersIds.move(indices[i],arr.length - 1 - i);
+        }
+    },[selection])
 
     const setFill = useMutation(({storage}, fill:Color)=>{
         const liveLayers = storage.get("layers");
@@ -51,6 +87,19 @@ const SelectionTools = memo(({camera, setLastUsedColor}:SelectionToolsProps) => 
         <ColorPicker 
         onChange={setFill}
         />
+
+        <div className="flex flex-col gap-y-0.5">
+            <Hint label="Вперед">
+                <Button onClick={moveToFront} variant="board" size="icon">
+                    <BringToFront />
+                </Button>
+            </Hint>
+            <Hint label="Назад" side="bottom">
+                <Button onClick={moveToBack} variant="board" size="icon">
+                    <SendToBack />
+                </Button>
+            </Hint>
+        </div>
         
         <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
             <Hint label="Delete" >
